8868afe69297eceac569fcb325fa0e36c2e865c9
alpdk <alexander@podkopaev.net>
2024-03-12 13:41:40 +0200

    Some small modifications

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -6,13 +6,15 @@ void initializeGL() {
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Setting black color as a background color
 }
 
+/* Roate object by an angel */
+
 void rotate(float angel) {
     glRotatef(angel, 0.0f, 0.0f, 1.0f);
 }
 
 /* Draw square function */
 
-void drawSquare(double x1, double y1, double sidelength) {
+void draw_square(double x1, double y1, double sidelength) {
     double halfside = sidelength / 2;
 
     glColor3d(1, 1, 1);
@@ -31,24 +33,52 @@ void drawSquare(double x1, double y1, double sidelength) {
     glEnd();
 }
 
-/* Display function */
+/* Display square function */
 
-void display2() {
+void display_square() {
     glClear(GL_COLOR_BUFFER_BIT);          // Clear the color buffer with current clearing color
 
-    drawSquare(0, 0, 1);        // Draw square with center in 0, 0 with side 1
+    draw_square(0, 0, 1);        // Draw square with center in 0, 0 with side 1
 
     glFlush();                                   // Render now
+}
+
+void draw_triangle() {
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(-0.1f, -0.2f, 0.0f);
+    glRotatef(60.0f, 0.0f, 1.0f, 0.0f);
+    glTranslatef(0.1f, 0.2f, 0.0f);
+    glRotatef(-60.0f, 0.0f, 1.0f, 0.0f);
+
+    glBegin(GL_LINE_LOOP);
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(0.1,0.3);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(0,0.1);
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glVertex2f(0.2,0.1);
+
+    glEnd();
+}
+
+void display_triangle() {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    draw_triangle();
+
+    glFlush();
 }
 
 /* Main function */
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv);               // Initialize GLUT
-    glutCreateWindow("Triangle");         // Create window with the given title
+    glutCreateWindow("Some figures");         // Create window with the given title
     glutInitWindowSize(320, 320); // Set the window's width & height
     glutInitWindowPosition(50, 50);       // Install top-left corner of the winfow
-    glutDisplayFunc(display2);         // Register callback handler for window re-paint event
+    glutDisplayFunc(display_triangle);         // Register callback handler for window re-paint event
     initializeGL();                           // Our own OpenGL initialization
     glutMainLoop();                           // Enter the event-processing loop
     return 0;
