1cd00062014efb80f0d3b22476f7731484c1d59e
alpdk <alexander@podkopaev.net>
2024-03-12 09:33:27 +0200

    Added implementation of the square

---
+++ Lab1/main.cpp
@@ -0,0 +1,48 @@
+#include <GL/glut.h>
+
+/* Initialize background color */
+
+void initializeGL() {
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Setting black color as a background color
+}
+
+/* Draw square function */
+
+void drawSquare(double x1, double y1, double sidelength)
+{
+    double halfside = sidelength / 2;
+
+    glColor3d(1,1,1);
+    glBegin(GL_POLYGON);
+
+    glVertex2d(x1 + halfside, y1 + halfside);
+    glVertex2d(x1 + halfside, y1 - halfside);
+    glVertex2d(x1 - halfside, y1 - halfside);
+    glVertex2d(x1 - halfside, y1 + halfside);
+
+    glEnd();
+}
+
+/* Display function */
+
+void display2() {
+    glClear(GL_COLOR_BUFFER_BIT);          // Clear the color buffer with current clearing color
+
+    drawSquare(0, 0, 1);        // Draw square with center in 0, 0 with side 1
+
+    glFlush();                                   // Render now
+}
+
+/* Main function */
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);               // Initialize GLUT
+    glutCreateWindow("Triangle");         // Create window with the given title
+    glutInitWindowSize(320, 320); // Set the window's width & height
+    glutInitWindowPosition(50, 50);       // Install top-left corner of the winfow
+    glutDisplayFunc(display2);         // Register callback handler for window re-paint event
+    initializeGL();                           // Our own OpenGL initialization
+    glutMainLoop();                           // Enter the event-processing loop
+    return 0;
+}
+
